1bc610f929d9ae98178f76dcb648ea12a2adb302
MarcQLF <marcbadouqlf@gmail.com>
2025-04-27 19:54:53 -0500

    Add project files (Assets, ProjectSettings, Packages)

---
+++ FPS ZOMBIE GAMEEE/Assets/SoundController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource shot;
+    //Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        shot.Play();
+    }
+}
